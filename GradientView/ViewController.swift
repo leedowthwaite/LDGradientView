7dd3765341d2ed2239c83a0e4478e631b61f7701
Lee Dowthwaite <leedowthwaite@gmail.com>
2017-12-12 22:12:35 +0000

    Added LD prefix and MIT license for public repo.

--- GradientView/ViewController.swift
+++ GradientView/ViewController.swift
@@ -1,26 +1,38 @@
 //
-//  ViewController.swift
-//  GradientView
+//    ViewController.swift
+//    LDGradientView
 //
-//  Created by Lee Dowthwaite on 10/12/2017.
-//  Copyright © 2017 Echelon Developments Ltd. All rights reserved.
+//    MIT License
 //
+//    Copyright (c) 2017 Lee Dowthwaite.
+//
+//    Permission is hereby granted, free of charge, to any person obtaining a copy
+//    of this software and associated documentation files (the "Software"), to deal
+//    in the Software without restriction, including without limitation the rights
+//    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//    copies of the Software, and to permit persons to whom the Software is
+//    furnished to do so, subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included in all
+//    copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//    SOFTWARE.
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var gradientView: GradientView!
-    //@IBOutlet weak var gradientView3: GradientView!
+    @IBOutlet weak var LDGradientView: LDGradientView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        
-//        let gradientView2 = GradientView(frame: CGRect(x: 50, y: 50, width: 100, height: 100))
-//        gradientView2.startColor = UIColor.blue
-//        self.view.addSubview(gradientView2)
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -31,7 +43,5 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
